05d2ce74b45240ef743ba5f6f814734cb5dc719c
Reena <reena@quintype.com>
2019-02-20 16:00:39 +0530

    design added

---
+++ src/todo-app/todo-app.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { TodoList } from "./todo-list/todo-list";
+import { DoneList } from "./done-list/done-list";
+import './todo-app-module.css'
+
+export class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoListItems: [],
+      doneListItems: [],
+      // todoInput: ''
+    }
+  }
+
+  handleTodoListChange = (todoListItems, doneValue) => {
+    const clonedDoneArray = this.state.doneListItems.map(item => item);
+    clonedDoneArray.push(doneValue);
+    this.setState({ doneListItems: clonedDoneArray, todoListItems: todoListItems });
+  };
+  handleDoneListChange = (doneListItems, doneValue) => {
+    this.setState({ doneListItems: doneListItems });
+  }
+  // handleTodoInput = event => {
+  //   if (this.state.todoInput === '') {
+  //     return;
+  //   }
+  //   const clonedTodoArray = this.state.todoListItems.map(item => item);
+
+  //   clonedTodoArray.push(this.state.todoInput);
+
+  //   this.setState({ todoListItems: clonedTodoArray,todoInput: '' });
+  // };
+  changeValue = () => {
+    let val = prompt("enter value");
+    if (!val)
+      return;
+    this.setState({ todoListItems: [val, ...this.state.todoListItems] });
+  }
+  // handleInputOnChange = event => {
+  //   this.setState({ todoInput: event.target.value });
+  // };
+  render() {
+    return <div>
+      {/* <input type={"text"} value={this.state.todoInput} onChange={this.handleInputOnChange} />
+  <button onClick={this.handleTodoInput}>Submit</button> */}
+      <div className="replaceTodoDone">
+        <h1>ToDo
+        </h1>
+        <TodoList
+          handleTodoListChange={this.handleTodoListChange}
+          todoListItems={this.state.todoListItems} />
+        <h1 className={"replaceDone"}>
+          Done
+        </h1>
+        <DoneList
+          handleDoneListChange={this.handleDoneListChange}
+          doneListItems={this.state.doneListItems} />
+        <button
+          onClick={this.changeValue}>+
+        </button>
+      </div>
+    </div>
+  }
+}
---
+++ src/todo-app/todo-list/todo-list.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './todo-list-module.css';
+
+
+export const TodoList = props => {
+  const onClickHandler = (event, clickedIndex) => {
+    const filteredTodoList = props.todoListItems.filter((item, index) => index !== clickedIndex);
+    props.handleTodoListChange(filteredTodoList, props.todoListItems[clickedIndex]);
+  };
+
+  return <div className={'todoList'}>
+    <ul>
+      {
+        props.todoListItems.map((item, index) =>
+          <li key={`todo-${index}`} >{item}
+           <div>
+              <button onClick={event => 
+              onClickHandler(event, index)} 
+              className= {'replaceButton'}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" viewBox="0 0 15 12">
+                <path fill="none" fill-rule="nonzero" stroke="#979797" d="M.6 6l2.773 5.333L14.467.667" />
+              </svg>
+            </button>
+            </div>
+          </li>)
+      }
+    </ul>
+  </div>
+}
